6eae2d996858f60e997a9c2264f874d031e3aa04
erladitia14 <erladitia14@upi.edu>
2026-04-26 21:03:38 +0700

    Update

--- ruffier.py
+++ ruffier.py
@@ -1,34 +1,33 @@
-''' Module for calculating the results of Ruffier tests.
+''' Module for calculating the results of the Ruffier tests.
  
-The sum of the three tries at pulse readings (before strain, right after strain, and after a short break)
+The sum of the three attempts at heart rate readings (before exertion, right after exertion, and after a short break)
 ideally, there should be no more than 200 beats per minute.
-We propose that the children measure their pulse for 15 seconds,
-and find the result of beats per minute by multiplying by 4:
+We suggest that the children measure their heart rate for 15 seconds,
+and we get the result of beats per minute by multiplying by 4:
    S = 4 * (P1 + P2 + P3)
 The further the result is from the ideal 200 beats, the worse it is.
-Traditionally, tables are given by values divided by 10.
+Traditionally, tables are given for the values divided by 10.
  
 Ruffier index  
-   IR = (S - 200) / 10
-is evaluated corresponding to age according to the table:
-        7-8             9-10                11-12               13-14               15+ (only for adolescents!)
-
-perfect     < 6.5           < 5                 < 3.5               < 2                 < 0.5  
-good    >= 6.5 и < 12   >= 5 и < 10.5       >= 3.5 и < 9        >= 2 и < 7.5        >= 0.5 и < 6
-satisfactory  >= 12 и < 17    >= 10.5 и < 15.5    >= 9 и < 14         >= 7.5 и < 12.5     >= 6 и < 11
-weak  >= 17 и < 21    >= 15.5 и < 19.5    >= 14 и < 18        >= 12.5 и < 16.5    >= 11 и < 15
-unsatisfactory   >= 21           >= 19.5             >= 18               >= 16.5             >= 15
-
-the result “unsatisfactory” is 4 from the result “weak” for all ages,
-“weak” is separated from “satisfactory” by 5, and “good” from “satisfactory” by 5.5
+   RI = (S - 200) / 10
+is evaluated according to age according to the table:
+       7–8             9–10                11–12               13–14               15+ (only for adolescents!)
+perfect    6.4 and below   4.9 and below       3.4 and below         1.9 and below               0.4 and below
+good    6.5–11.9     5–10.4          3.5–8.9           2–7.4                   0.5–5.9
+satisfactory  12–16.9      10.5–15.4       9–13.9            7.5–12.4                6–10.9
+weak  17–20.9      15.5–19.4       14–17.9           12.5–16.4               11–14.9
+unsatisfactory   21 and above     19.5 and above      18 and above          16.5 and above             15 and above
+ 
+the result “unsatisfactory” is 4 away from the result “weak” for all ages,
+“weak” is 5 away from “satisfactory,” and “good” is 5.5 away from “satisfactory,”
  
 so we will write a function ruffier_result(r_index, level) which will produce
-the calculated Ruffier index and level “unsatisfactory” for the tested age, and produce a result
+the calculated Ruffier index and “unsatisfactory” level for the tested age, and produce a result
  
 '''
 # here the lines which produce the result are given
 txt_index = "Your Ruffier index: "
-txt_workheart = "Heart efficiency: "
+txt_workheart = "Heart performance: "
 txt_nodata = '''
 there is no data for that age'''
 txt_res = []
@@ -37,12 +36,12 @@ Go see your doctor ASAP!''')
 txt_res.append('''satisfactory.
 Go see your doctor!''')
 txt_res.append('''average.
-It might be worth additional tests at the doctor.''')
+It might be worth doing additional tests at the doctor.''')
 txt_res.append('''
 higher than average''')
 txt_res.append('''
 high''')
-
+ 
 def ruffier_index(P1, P2, P3):
    ''' it returns the index value according to the three pulse calculations for comparison with the table'''
    S = 4 * (P1 + P2 + P3)
